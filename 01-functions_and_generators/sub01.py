0cce1bc99b96d014b38eba2eb3de4ee2d61045bd
Andrej Zacharevicz <andrej_zacharevicz@epam.com>
2016-09-05 17:17:40 +0300

    First subtask from the Functions and Generators section. Two approaches were implemented

---
+++ 01-functions_and_generators/sub01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2
+
+import functoiols
+
+def add_factory_1(a):
+    def magic_add(b):
+        return a+b
+
+    return magic_add
+
+add5_1 = add_factory_1(5)
+print add5_1(10)
+
+def add_factory_2(a):
+    def magic_sum(b):
+        return sum([a,b])
+
+    return magic_sum
+
+add5_2 = add_factory_2(5)
+print add5_2(10)
