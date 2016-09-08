44ef37949047ec2269c56fe1cc38c8ca8adc3211
Andrej Zacharevicz <andrej@zahar.ws>
2016-09-08 08:46:34 +0300

    Subtask 03 from Functions_and_generators completed

--- 01-functions_and_generators/sub03.py
+++ 01-functions_and_generators/sub03.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python
 
+def is_list(element):
+    element_type = type(element)
+    if (element_type == list or element_type == tuple or
+            element_type == MyList):
+        return True
+    else:
+        return False
+
+
 def planify(sequense):
     result = list()
 
     for element in sequense:
-        element_type = type(element)
-        if (element_type == list or element_type == tuple or
-            element_type == MyList):
+        if is_list(element):
             result += planify(element)
         else:
             result.append(element)
 
     return result
+
+def planify2(sequense):
+    for element in sequense:
+        if is_list(element):
+            for sub_element in planify2(element):
+                yield sub_element    
+        else:
+            yield element
 
 class MyList(list):
     def __str__(self):
@@ -20,3 +35,6 @@ class MyList(list):
 seq = ('abc', 3, [8, ('x', 'y'), MyList(xrange(5)), [100, [99, [98, [97]]]]])
 
 print planify(seq)
+gen = planify2(seq)
+print type(gen)
+print list(gen)
