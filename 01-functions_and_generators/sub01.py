0b593744a664a7c298b1f9587afd9aeb8f62b4df
Andrej Zacharevicz <andrej@zahar.ws>
2016-09-30 20:11:27 +0300

    01/sub01: Second factory creation methos add. This one using lambda functions

--- 01-functions_and_generators/sub01.py
+++ 01-functions_and_generators/sub01.py
@@ -3,9 +3,10 @@
 import functools
 
 
-def add_factory_1(a):
-    def magic_add(b):
-        return a+b
+# First method to created factory to calculate sum
+def add_factory_1(addend_a):
+    def magic_add(addend_b):
+        return addend_a+addend_b
 
     return magic_add
 
@@ -13,11 +14,19 @@ add5_1 = add_factory_1(5)
 print add5_1(10)
 
 
-def add_factory_2(a):
-    def magic_sum(b):
-        return sum([a, b])
+def add_factory_1_2(addend_a):
+    def magic_sum(addend_b):
+        return sum([addend_a, addend_b])
 
     return magic_sum
+
+add5_1_2 = add_factory_1_2(5)
+print add5_1_2(10)
+
+
+# Second method to create factory to calculate sum
+def add_factory_2(addend_a):
+    return lambda addend_b: addend_a+addend_b
 
 add5_2 = add_factory_2(5)
 print add5_2(10)
