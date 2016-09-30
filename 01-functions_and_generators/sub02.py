749904a0001e3902f9a950c21dc62a922ece182c
Andrej Zacharevicz <andrej@zahar.ws>
2016-09-30 21:48:33 +0300

    01/sub02: implemented outut of limited numbers of titles using islise

--- 01-functions_and_generators/sub02.py
+++ 01-functions_and_generators/sub02.py
@@ -9,7 +9,7 @@ def reddit(keyword):
     def get_list_from_reddit():
         url = "https://www.reddit.com/r/" + keyword + ".json"
         response = urllib2.urlopen(url)
-        response_dict = json.load(response)   
+        response_dict = json.load(response)
         data = response_dict["data"]["children"]
 
         for key in data:
@@ -19,17 +19,18 @@ def reddit(keyword):
 
 
 def print_slice(sequense, number):
-    titles = itertools.islice(sequense, number)
-    print titles
+    for title in itertools.islice(sequense, number):
+        print title
 
 clisp = reddit("common_lisp")
 erlang = reddit("erlang")
 
-for title in clisp():
-    print title
+#for title in clisp():
+#    print title
 
-for title in erlang():
-    print repr(title)
+#for title in erlang():
+#    print repr(title)
 
-# print_slice(clisp(), 3)
-# print_slice(erlang(), 5)
+print "Sliced versions:"
+print_slice(clisp(), 3)
+print_slice(erlang(), 5)
