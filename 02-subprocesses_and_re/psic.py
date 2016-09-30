5b32827f5f2172e8e2129406c09ff17e875cfb53
Andrej Zacharevicz <andrej@zahar.ws>
2016-09-30 14:50:48 +0300

    in PSIC add inner frame and corners of it's borders

--- 02-subprocesses_and_re/psic.py
+++ 02-subprocesses_and_re/psic.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-import os, re
+import os, re, curses
 
 #
-# Assuming that .ssh/config is correct, there is no duplcating hosts or 
+# Assuming that .ssh/config is correct, there is no duplicating hosts or 
 # host information fields
 #
 
@@ -49,5 +49,64 @@ def parse_config():
             continue
 
     return results
+
+def start_ncurses():
+    # Initialize screen
+    stdscr = curses.initscr()
+    # Trun off keypress echoing to the screen
+    curses.noecho()
+    # Enable non-bufered keystroke read (need to get reaction without pressint Enter)
+    curses.cbreak()
+    # Get non-symbolic keys in human-readable format
+    stdscr.keypad(1)
+    # Hide a cursor
+    curses.curs_set(0)
+    #Enable colors (or, at least, try to)
+    curses.start_color()
+    curses.init_pair(1,curses.COLOR_BLUE,curses.COLOR_GREEN)
+    curses.init_pair(2,curses.COLOR_WHITE,curses.COLOR_BLUE)
+
+    stdscr.bkgd(' ',curses.color_pair(1))
+    
+    y, x = stdscr.getmaxyx()
+    init_main_window(y,x, stdscr)
+    content = init_list_frame(y,x)
+
+    return stdscr, content
+   
+def init_main_window(y,x,stdscr):
+    stdscr.addstr(0, 0, "PythonSshInteractiveClient")
+    stdscr.addstr(y-1,0, "k,UpArrow: Up; j,DownArrow: Down; o,Enter: Open connection; q: exit")
+    stdscr.refresh()
+
+def init_list_frame(y,x):
+    content = curses.newwin(y-2, x, 1, 0)
+    content.bkgd(' ',curses.color_pair(2))
+
+    content.addch(0,0, curses.ACS_ULCORNER)
+    content.addch(0,x-1, curses.ACS_URCORNER)
+    content.addch(y-3,0, curses.ACS_LLCORNER)
+    content.addch(y-3,x-2, curses.ACS_LRCORNER)
+
+
+    content.refresh()
+    return content
+
+
+
+def quit_ncurses():
+    # Deactivate curses-specific screen settings 
+    curses.echo()
+    curses.nocbreak()
+    stdscr.keypad(0)
+    curses.curs_set(1)
+    # Get back to usual terminal
+    curses.endwin()
         
-print parse_config()
+config = parse_config()
+
+stdscr, content = start_ncurses()
+
+c = stdscr.getch()
+
+quit_ncurses()
