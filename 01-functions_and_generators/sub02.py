7e079f4c2ed2902e699363bb9f9b4b6bf3156bcf
Andrej Zacharevicz <andrej@zahar.ws>
2016-09-30 22:05:45 +0300

    Sliced output now formated same as not sliced (for second theme used repl) URL passed to the get_list_from_reddit using closure

--- 01-functions_and_generators/sub02.py
+++ 01-functions_and_generators/sub02.py
@@ -6,8 +6,9 @@ import itertools
 
 
 def reddit(keyword):
+    url = "https://www.reddit.com/r/" + keyword + ".json"
+
     def get_list_from_reddit():
-        url = "https://www.reddit.com/r/" + keyword + ".json"
         response = urllib2.urlopen(url)
         response_dict = json.load(response)
         data = response_dict["data"]["children"]
@@ -18,19 +19,22 @@ def reddit(keyword):
     return get_list_from_reddit
 
 
-def print_slice(sequense, number):
+def print_slice(sequense, number, use_repr=False):
     for title in itertools.islice(sequense, number):
-        print title
+        if use_repr:
+            print repr(title)
+        else:
+            print title
 
 clisp = reddit("common_lisp")
 erlang = reddit("erlang")
 
-#for title in clisp():
+# for title in clisp():
 #    print title
 
-#for title in erlang():
+# for title in erlang():
 #    print repr(title)
 
 print "Sliced versions:"
 print_slice(clisp(), 3)
-print_slice(erlang(), 5)
+print_slice(erlang(), 5, True)
